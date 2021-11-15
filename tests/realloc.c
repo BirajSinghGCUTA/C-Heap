08ee6dc464cf6915907ba638a02765e77da891eb
BirajSinghGCUTA <bsg7347@mavs.uta.edu>
2021-11-15 01:52:09 -0600

    fix

--- tests/realloc.c
+++ tests/realloc.c
@@ -4,13 +4,21 @@
 
 int main()
 {
-   int *ptr = (int *)malloc(sizeof(int)*10);
-   
-   *ptr = 10;
-   
+   int *ptr = (int *)malloc(sizeof(int)*2);
+   int i;
+   int *ptr_new;
 
-   int * ptr_new = (int *)realloc(ptr, sizeof(int)*1);
-   *ptr_new = 1;
+   *ptr = 10;
+   *(ptr + 1) = 20;
+
+   ptr_new = (int *)realloc(ptr, sizeof(int)*3);
+   *(ptr_new + 2) = 30;
+
+   for(i = 0; i < 3; i++)
+   {
+     assert( *(ptr_new + i ) == ( i + 1 ) * 10 );
+   }
+
    printf("realloc test PASSED\n");
 
    return 0;
